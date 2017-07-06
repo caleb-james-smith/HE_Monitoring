a063db4a1a083ea5330c918b2052a972ad848ae8
Caleb James Smith <colorado.caleb.smith@gmail.com>
2017-07-06 10:48:08 +0200

    Plot value vs RM and RM vs value.

--- plot.py
+++ plot.py
@@ -8,16 +8,31 @@ def plot(filename,unit):
   for line in data:
     parsed = line.split(" ")
     if parsed[0] == "#":  continue
-    print parsed
+    #print parsed
     val = float((parsed[-1].split("\n"))[0])
     values.append(val)
   
+  data.close()
   
+  vmin, vmax = 0.0, 10.0
+
+  # line graph (value vs readout modudle)
   plt.plot(values)
+  plt.axis([0,72,vmin,vmax])
   plt.ylabel(unit)
   plt.xlabel("Readout Module")
+  plt.title("Monitoring "+unit)
+  plt.grid(True)
   plt.show()
-  data.close()
+  
+  # histogram (count vs value)
+  n, bins, patches = plt.hist(values, 20, facecolor='g', alpha=0.75)
+  plt.axis([vmin,vmax,0,20])
+  plt.ylabel("Number of Readout Modules")
+  plt.xlabel(unit)
+  plt.title("Monitoring "+unit)
+  plt.grid(True)
+  plt.show()
 
 if __name__ == "__main__":
   plot("humidity-5-JUL-2017.txt","Relative Humidity")
