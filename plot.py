65ffcdd920ea99ff89d7764e384623e36666c412
Caleb James Smith <caleb_smith2@baylor.edu>
2017-07-11 17:50:02 +0200

    Edit labels for temperature plots.

--- plot.py
+++ plot.py
@@ -6,18 +6,17 @@ import imageio
 
 def plot(filename,unit,plot_directory):
   data_directory = "measurements/"
-  data = open(data_directory+filename+'.txt','r')
+  
   values = []
   num_rm = 0
-  for line in data:
-    parsed = line.split(" ")
-    if parsed[0] == "#":  continue
-    #print parsed
-    val = float((parsed[-1].split("\n"))[0])
-    values.append(val)
-    num_rm += 1
-  
-  data.close()
+  with open(data_directory+filename+'.txt','r') as data:
+    for line in data:
+      parsed = line.split(" ")
+      if parsed[0] == "#":  continue
+      #print parsed
+      val = float((parsed[-1].split("\n"))[0])
+      values.append(val)
+      num_rm += 1
   
   mean = np.round(np.mean(values), 3)
   std  = np.round(np.std(values), 3)
@@ -32,9 +31,12 @@ def plot(filename,unit,plot_directory):
   
   # line graph (value vs readout modudle)
   plt.plot(values)
-  plt.text(10, 9, filename)
-  plt.text(10, 9, filename)
-  plt.text(40, 9, r'$\mu='+str(mean)+',\ \sigma='+str(std)+'$')
+  if "Temperature" in unit:
+    plt.text(10, 45, filename)
+    plt.text(40, 45, r'$\mu='+str(mean)+',\ \sigma='+str(std)+'$')
+  if "Humidity" in unit:
+    plt.text(10, 9, filename)
+    plt.text(40, 9, r'$\mu='+str(mean)+',\ \sigma='+str(std)+'$')
   plt.axis([0,num_rm,vmin,vmax])
   plt.ylabel(unit)
   plt.xlabel("Readout Module")
@@ -46,8 +48,12 @@ def plot(filename,unit,plot_directory):
   
   # histogram (count vs value)
   n, bins, patches = plt.hist(values, 20, facecolor='g', alpha=0.75)
-  plt.text(2, 18, filename)
-  plt.text(2, 16, r'$\mu='+str(mean)+',\ \sigma='+str(std)+'$')
+  if "Temperature" in unit:
+    plt.text(2, 18, filename)
+    plt.text(20, 18, r'$\mu='+str(mean)+',\ \sigma='+str(std)+'$')
+  if "Humidity" in unit:
+    plt.text(2, 18, filename)
+    plt.text(6, 18, r'$\mu='+str(mean)+',\ \sigma='+str(std)+'$')
   plt.axis([vmin,vmax,0,20])
   plt.ylabel("Number of Readout Modules")
   plt.xlabel(unit)
@@ -56,6 +62,8 @@ def plot(filename,unit,plot_directory):
   #plt.show()
   fig.savefig(plot_directory+filename+"_2.png")
   plt.clf()
+
+  plt.close('all')
 
 def makeGif(filenames, gifname, unit):
   plot_directory = "images/"
@@ -68,21 +76,21 @@ def makeGif(filenames, gifname, unit):
 
 def getFiles(file_set):
   file_list = []
-  files = open(file_set,'r')
-  for f in files:
-    line = f.split('\n')[0]
-    file_list.append(line)
-    print line
-  return file_list
+  with open(file_set,'r') as files:
+    for f in files:
+      line = f.split('\n')[0]
+      file_list.append(line)
+      print line
+    return file_list
 
 if __name__ == "__main__":
   #plot("humidity-5-JUL-2017","Relative Humidity")
   #plot("temperature-5-JUL-2017","Temperature deg C")
+  
   files = getFiles("humidity_files.txt")
   makeGif(files,"humidity","Relative Humidity")
-
-
-
-
+  
+  files = getFiles("temperature_files.txt")
+  makeGif(files,"temperature","Temperature deg C")
 
 
