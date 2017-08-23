62e3ffa9f347c944977d3eea7a1b2cd0fa2dd076
Caleb Smith <caleb@cern.ch>
2017-08-23 13:25:21 +0200

    editing command list and ngfec auto to read HB temperature and humidities over time

--- ngfec_auto.py
+++ ngfec_auto.py
@@ -12,9 +12,11 @@
 #######################################################################
 
 import sys
+#import makePlots as mp
 from sendCommands import *
 from argparse import ArgumentParser
 import re
+import time
 
 # Regex to search for floats 
 # (?<!_)    NOT preceeded by _
@@ -24,25 +26,42 @@ r = re.compile(r"(?<!_)\d+\.\d+")
 
 parser = ArgumentParser()
 parser.add_argument("cmds", help="text file containing list of ngFEC commands")
+parser.add_argument("time", help="total time for measurement")
 args = parser.parse_args()
 
 cmdList = []
 with open(args.cmds, 'r') as f:
     for line in f:
-	l = line.strip()
-	if l != "":	# Only consider non-empty lines
-    	    cmdList.append(line.strip())
+        l = line.strip()
+        if l != "":	# Only consider non-empty lines
+            cmdList.append(line.strip())
 
-results = send_commands(cmds=cmdList,script=True,port=64000,control_hub='hcal904daq04')
+results = send_commands(cmds=cmdList,script=True,port=64400,control_hub='hcal904daq04')
 
+date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 print "-------------------------------------"
-print "| ngFEC output:\t%s |" % datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print "| ngFEC output:\t%s |" % date 
 print "-------------------------------------"
-for line in results:
-    # Extracts all the float values from a command output into a list
-    vals = r.findall(line['result'])
-    for val in vals:
-	print val   
+runtime = 0
+step = 5 # seconds 
+totaltime = int(args.time)
+print "total time: {0}".format(totaltime)
+while runtime < totaltime:
+    for i, line in enumerate(results):
+        print line['cmd']
+        print "time: {0}".format(runtime)
+        # Extracts all the float values from a command output into a list
+        vals = r.findall(line['result'])
+        for val in vals:
+            print val
+        '''
+        if i == 0:
+            mp.plot(date, vals, "Relative Humidity", "images/")
+        elif i == 1:
+            mp.plot(date, vals, "Temperature in deg C", "images/")
+        '''
+    time.sleep(step)
+    runtime += step
 
 
 
