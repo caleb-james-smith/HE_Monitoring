eedb65fa8e5de5760c69025157bc69c5969b0331
Caleb James Smith <caleb_smith2@baylor.edu>
2017-10-11 19:24:55 +0200

    update torch scripts with ssh so that they run from hcal904daq01

--- torch_i.sh
+++ torch_i.sh
@@ -1,4 +1,10 @@
 logfile="torch.log"
-echo "V2O?;I2O?" | nc localhost 39221
-echo "$(date) Read voltage and current." >> $logfile
-echo "V2O?;I2O?" | nc localhost 39221 >> $logfile
+host="cms904usr"
+dir="$(pwd)"
+
+ssh -T $host << EOF
+  cd $dir
+  echo "V2O?;I2O?" | nc localhost 39221
+  echo "$(date) Read voltage and current." >> $logfile
+  echo "V2O?;I2O?" | nc localhost 39221 >> $logfile
+EOF
--- torch_v.sh
+++ torch_v.sh
@@ -1,7 +1,12 @@
 logfile="torch.log"
-echo "V2V $1" | nc localhost 39221
-sleep 1
-echo "V2O?; I2O?" | nc localhost 39221
+host="cms904usr"
+dir="$(pwd)"
 
-echo "$(date) Set voltage to $1" >> $logfile
-echo "V2O?;I2O?" | nc localhost 39221 >> $logfile
+ssh -T $host << EOF
+  cd $dir
+  echo "V2V $1" | nc localhost 39221
+  sleep 1
+  echo "V2O?; I2O?" | nc localhost 39221
+  echo "$(date) Set voltage to $1" >> $logfile
+  echo "V2O?;I2O?" | nc localhost 39221 >> $logfile
+EOF
